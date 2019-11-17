b495a6f0e71fd634ae80228aa7f886eb7f8fd6a2
Gopal <gopalpenny@gmail.com>
2019-11-17 18:07:19 -0500

    updates

--- R/anem_geoprocessing.R
+++ R/anem_geoprocessing.R
@@ -79,65 +79,60 @@ prep_bounds <- function(boundaries,get_rectangular=FALSE) {
   if (!max(grepl("^bID$",names(boundaries)))) { # generate pID's if they are not present
     boundaries <- boundaries %>% dplyr::mutate(bID=dplyr::row_number())
   }
-  # if (rectangular) {
-  #   boundaries <- get_rectangular(boundaries)
-  # }
-  bounds_w_slope <- boundaries %>%
-    dplyr::mutate(m=(y2-y1)/(x2-x1),
-           b=y1 - m*x1) %>% dplyr::select(-x1,-y1,-x2,-y2)
+  if (get_rectangular) {
+    bounds_w_slope <- get_rectangle(boundaries)
+  } else{
+    bounds_w_slope <- boundaries %>%
+      dplyr::mutate(m=(y2-y1)/(x2-x1),
+                    b=y1 - m*x1) %>% dplyr::select(-x1,-y1,-x2,-y2)
+  }
   return(bounds_w_slope)
 }
-
-#' #' Get intersection of two lines
-#' #'
-#' #' Get the intersection of two lines described by m, b
-#' get_intersection()
-#'
-#'
 
 
 #' Convert quadrangle to rectangle
 #'
 #' Convert 4 lines to a rectangular box with only right angles
-#' @param boundaries a data.frame containing 4 lines (rows) defined, respectively, by x1, y1, x2, y2
+#' @param boundaries a \code{data.frame} containing 4 lines (rows) defined, respectively, by x1, y1, x2, y2, and an id (bID)
 #' @importFrom magrittr %>%
-#' @return Returns a data.frame containing slope and intercept for four edges of a rectangle. The
+#' @return Returns a \code{data.frame} containing slope and intercept for four edges of a rectangle. The
 #' rectangle is determined by (1) identifying the quadrangle of the input \code{boundaries}, (2)
 #' selecting the midpoints of each edge of the quadrangle, (3) determining the "long" axis of the
 #' quadrangle, which becomes the long axis of the rectangle, (4) calculating the slope of the long
 #' and short axes of the rectangle (at right angles), then generating lines with these slopes through
 #' the midpoints.
 #' @examples
-#' boundaries <- data.frame(x1=c(0,10,13,1),y1=c(0,10,9,-1),x2=c(10,13,1,0),y2=c(10,9,-1,0))
-#' rect_boundaries <- get_rectangle(boundaries) %>% mutate(id=as.factor(row_number()))
+#' boundaries <- data.frame(x1=c(0,10,13,1),y1=c(0,10,9,-1),x2=c(10,13,1,0),y2=c(10,9,-1,0)) %>%
+#'   mutate(bID=row_number())
+#' rect_boundaries <- get_rectangle(boundaries)
 #' ggplot() + geom_segment(data=boundaries,aes(x1,y1,xend=x2,yend=y2)) +
-#'   geom_abline(data=rect_boundaries,aes(slope=m,intercept=b,color=id))
-get_rectangle <- function(boundaries){
+#'   geom_abline(data=rect_boundaries,aes(slope=m,intercept=b,color=as.factor(bID)))
+get_rectangle <- function(boundaries) {
   # get vertices
   quad_vertices_full <- get_quad_vertices(boundaries)
 
   # get midpoints of boundaries
-  midpoints <- quad_vertices_full %>% dplyr::group_by(bound_id) %>%
+  midpoints <- quad_vertices_full %>% dplyr::group_by(bID) %>%
     dplyr::summarize(x_mid=mean(x,na.rm=TRUE),
               y_mid=mean(y,na.rm=TRUE),
-              opposite_id=intersection_id[is.na(x)])
+              opposite_bID=intersection_bID[is.na(x)])
 
   # get slope of of long and short axes
   slopes <- midpoints %>%
-    dplyr::left_join(midpoints %>% dplyr::select(-bound_id) %>%
-                       dplyr::rename(bound_id=opposite_id,x_mid2=x_mid,y_mid2=y_mid)) %>%
+    dplyr::left_join(midpoints %>% dplyr::select(-bID) %>%
+                       dplyr::rename(bID=opposite_bID,x_mid2=x_mid,y_mid2=y_mid)) %>%
     dplyr::mutate(dist=sqrt((x_mid2-x_mid)^2+(y_mid2-y_mid)^2))%>%
     dplyr::filter(dist==max(dist)) %>% dplyr::slice(1) %>%
     dplyr::mutate(m=(y_mid2-y_mid)/(x_mid2-x_mid),
                   m2=-1/m) %>%
-    tidyr::gather(var,bound_id,dplyr::ends_with("_id")) %>%
-    dplyr::select(bound_id,m,m2)
+    tidyr::gather(var,bID,dplyr::ends_with("bID")) %>%
+    dplyr::select(bID,m,m2)
 
   # get boundaries as m, b
   bounds_rectangular <- midpoints %>%
-    dplyr::left_join(slopes %>% dplyr::select(-m) %>% dplyr::rename(m=m2),by="bound_id") %>%
+    dplyr::left_join(slopes %>% dplyr::select(-m) %>% dplyr::rename(m=m2),by="bID") %>%
     dplyr::mutate(m=dplyr::if_else(!is.na(m),m,slopes$m[1])) %>%
-    dplyr::mutate(b=y_mid - m*x_mid) %>% dplyr::select(m,b)
+    dplyr::mutate(b=y_mid - m*x_mid) %>% dplyr::select(m,b,bID)
 
   return(bounds_rectangular)
 }
@@ -151,44 +146,42 @@ get_rectangle <- function(boundaries){
 #' are given an id, and there is one row in the output which identifies the opposide bound
 #' (with which there is no intersection).
 #' @examples
-#' boundaries <- data.frame(x1=c(0,10,13,1),y1=c(0,10,9,-1),x2=c(10,13,1,0),y2=c(10,9,-1,0))
+#' boundaries <- data.frame(x1=c(0,10,13,1),y1=c(0,10,9,-1),x2=c(10,13,1,0),y2=c(10,9,-1,0)) %>% mutate(bID=row_number())
 #' quad_vertices <- get_quad_vertices(boundaries)
 #' ggplot() + geom_segment(data=boundaries,aes(x1,y1,xend=x2,yend=y2)) +
-#'   geom_point(data=quad_vertices,aes(x,y,shape=as.factor(bound_id),color=as.factor(bound_id)),size=4) +
+#'   geom_point(data=quad_vertices,aes(x,y,shape=as.factor(bID),color=as.factor(bID)),size=4) +
 #'   scale_shape_manual(values=1:4)
 get_quad_vertices <- function(boundaries) {
   bounds_w_slope <- boundaries %>%
     dplyr::mutate(m=(y2-y1)/(x2-x1),
                   b=y1 - m*x1) %>% dplyr::select(-x1,-y1,-x2,-y2) %>%
-    dplyr::arrange(desc(m)) %>%
-    dplyr::mutate(slope_id=row_number())
-  bounds_w_slope <- bounds_w_slope[c(1,3,2,4),] %>% dplyr::mutate(bound_id=row_number()) %>% dplyr::select(-slope_id)
+    dplyr::arrange(desc(m))
   vertices <- bounds_w_slope %>%
-    tidyr::crossing(bounds_w_slope %>% dplyr::rename(m2=m,b2=b,bound_id2=bound_id)) %>%
+    tidyr::crossing(bounds_w_slope %>% dplyr::rename(m2=m,b2=b,bID2=bID)) %>%
     dplyr::mutate(x=round(get_intersection(m,b,m2,b2)$x,14), # round to avoid math errors that make points seem different
            y=round(get_intersection(m,b,m2,b2)$y,14)) %>%
-    dplyr::select(bound_id,bound_id2,x,y) %>% dplyr::filter(!is.nan(x))
+    dplyr::select(bID,bID2,x,y) %>% dplyr::filter(!is.nan(x))
   vertices_of_quad_midpoints <- vertices %>%
-    dplyr::group_by(bound_id) %>%
+    dplyr::group_by(bID) %>%
     dplyr::summarize(x=get_point_on_quandrangle(x),
               y=get_point_on_quandrangle(y)) %>%
-    dplyr::left_join(vertices,by=c("bound_id","x","y")) %>%
-    tidyr::gather(orig,bound_id,dplyr::starts_with("bound_id"))
+    dplyr::left_join(vertices,by=c("bID","x","y")) %>%
+    tidyr::gather(orig,bID,dplyr::starts_with("bID"))
   segments_remaining <- vertices_of_quad_midpoints %>%
     dplyr::select(-orig) %>%
     dplyr::distinct() %>%
-    dplyr::group_by(bound_id) %>%
+    dplyr::group_by(bID) %>%
     dplyr::summarize(n=dplyr::n()) %>%
     dplyr::filter(n==1)
   vertices_of_quad <- vertices %>%
-    dplyr::filter(bound_id==segments_remaining$bound_id[1],bound_id2==segments_remaining$bound_id[2]) %>%
-    tidyr::gather(orig,bound_id,dplyr::starts_with("bound_id")) %>%
+    dplyr::filter(bID==segments_remaining$bID[1],bID2==segments_remaining$bID[2]) %>%
+    tidyr::gather(orig,bID,dplyr::starts_with("bID")) %>%
     dplyr::bind_rows(vertices_of_quad_midpoints) %>%
-    dplyr::select(-orig,-bound_id) %>% dplyr::distinct()# %>% mutate(keep=TRUE)
+    dplyr::select(-orig,-bID) %>% dplyr::distinct()# %>% mutate(keep=TRUE)
   quad_vertices_full <- vertices %>% dplyr::inner_join(vertices_of_quad,by=c("x","y")) %>%
-    dplyr::rename(intersection_id=bound_id2) %>%
-    tidyr::complete(bound_id,intersection_id) %>%
-    dplyr::filter(bound_id!=intersection_id)
+    dplyr::rename(intersection_bID=bID2) %>%
+    tidyr::complete(bID,intersection_bID) %>%
+    dplyr::filter(bID!=intersection_bID)
 
   return(quad_vertices_full)
 }
@@ -209,13 +202,6 @@ get_point_on_quandrangle <- function(vec) {
   return(point)
 }
 
-# ggplot() +
-#   geom_segment(data=boundaries,aes(x1,y1,xend=x2,yend=y2)) +
-#   geom_point(data=vertices,aes(x,y)) +
-#   geom_point(data=midpoints,aes(x_mid,y_mid,color=as.factor(bound_id)),shape=15,size=3) +
-#   geom_point(data=vertices_of_quad,aes(x,y),shape=1,size=4) +
-#   geom_abline(data=bounds_rectangular,aes(slope=m,intercept=b,color=as.factor(bound_id)))
-
 #' Get intersection of two lines specified by slope m and intercept, b
 get_intersection <- function(m1,b1,m2,b2) {
   x <- (b2-b1)/(m1-m2)
@@ -223,13 +209,3 @@ get_intersection <- function(m1,b1,m2,b2) {
   intersections <- data.frame(x=x,y=y)
   return(intersections)
 }
-
-
-# get vertices
-
-# get mid points
-
-# get long axis
-
-# get slo
-
--- tests/testthat/test_geoprocessing.R
+++ tests/testthat/test_geoprocessing.R
@@ -14,7 +14,7 @@ test_that("utm_zone_to_proj4 returns correct proj4string for zone 32", {
 })
 
 
-boundaries <- data.frame(x1=c(0,10,13,1),y1=c(0,10,9,-1),x2=c(10,13,1,0),y2=c(10,9,-1,0))
+boundaries <- data.frame(x1=c(0,10,13,1),y1=c(0,10,9,-1),x2=c(10,13,1,0),y2=c(10,9,-1,0)) %>% dplyr::mutate(bID=row_number())
 test_that("get_rectangle returns data.frame from simple quadrangle",{
-  expect_equal(round(get_rectangle(boundaries),4),tibble::tibble(m=c(0.9091,-1.1,0.9091,-1.1),b=c(0.4545,22.15,-2.3636,0.05)))
+  expect_equal(round(get_rectangle(boundaries),4),tibble::tibble(m=c(0.9091,-1.1,0.9091,-1.1),b=c(0.4545,22.15,-2.3636,0.05),bID=as.numeric(1:4)))
 })
