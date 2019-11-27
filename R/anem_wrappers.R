4e4744fac50fb8505d39de06a466cedd7d461110
Gopal <gopalpenny@gmail.com>
2019-11-26 22:21:53 -0500

    get_gridded_hydrodynamics added

--- R/anem_wrappers.R
+++ R/anem_wrappers.R
@@ -33,7 +33,7 @@
 #' get_drawdown_relationships(wells, aquifer_unconfined, group_column=country, weights_column=weights)
 #'
 #' ggplot() +
-#'   geom_segment(data=aquifer_confined$bounds,aes(x1,y1,xend=x2,yend=y2,color=bound_type)) +
+#'   geom_segment(data=aquifer_unconfined$bounds,aes(x1,y1,xend=x2,yend=y2,color=bound_type)) +
 #'   geom_abline(slope=0,intercept=500,linetype="dashed") +
 #'   geom_point(data=wells %>% filter(wID==orig_wID),aes(x,y,fill=country),shape=21) +
 #'   coord_equal()
@@ -80,7 +80,7 @@ get_drawdown_relationships <- function(wells,aquifer,group_column,weights_column
 #' @examples
 #' # define aquifer
 #' bounds_df <- data.frame(bound_type=c("CH","NF","NF","NF"),m=c(Inf,0,Inf,0),b=c(0,1000,1000,0))
-#' aquifer_confined <- define_aquifer("unconfined",1e-3,bounds=bounds_df,h0=100)
+#' aquifer_unconfined <- define_aquifer("unconfined",1e-3,bounds=bounds_df,h0=100)
 #'
 #' # define wells and well images
 #' set.seed(30)
@@ -89,12 +89,12 @@ get_drawdown_relationships <- function(wells,aquifer,group_column,weights_column
 #'          country=factor(y>500,levels=c(F,T),labels=c("A","B"))) %>%
 #'   group_by(country) %>%
 #'   mutate(weights=1,Q=1/n()) %>% group_by()
-#' wells <- define_wells(wells_df) %>% generate_image_wells(aquifer_confined)
+#' wells <- define_wells(wells_df) %>% generate_image_wells(aquifer_unconfined)
 #'
-#' get_single_drawdown_relationship(wells, aquifer_confined, group_column=country, weights_column=weights,loc_group="S", pump_group="F")
+#' get_single_drawdown_relationship(wells, aquifer_unconfined, group_column=country, weights_column=weights,loc_group="S", pump_group="F")
 #'
 #' ggplot() +
-#'   geom_segment(data=aquifer_confined$bounds,aes(x1,y1,xend=x2,yend=y2,color=bound_type)) +
+#'   geom_segment(data=aquifer_unconfined$bounds,aes(x1,y1,xend=x2,yend=y2,color=bound_type)) +
 #'   geom_abline(slope=0,intercept=500,linetype="dashed") +
 #'   geom_point(data=wells %>% filter(wID==orig_wID),aes(x,y,fill=country),shape=21) +
 #'   coord_equal()
@@ -138,3 +138,74 @@ get_single_drawdown_relationship <- function(wells, aquifer, group_column, weigh
 
   return(weighted_potential)
 }
+
+
+#' Map hydrodynamics
+#'
+#' This function is used for mapping hydrodynamics by generating gridded output of
+#' hydraulic head and flow direction
+#' @inheritParams get_drawdown_relationships
+#' @param head_dim Vector containing number of rows and columns as \code{c(nrow,ncol)} for the grid of hydraulic head
+#' @param flow_dim Vector containing number of rows and columns as \code{c(nrow,ncol)} for the grid of flow
+#' @return
+#' The function returns a list containing:
+#' \itemize{
+#'   \item{\code{head}: }{A data.frame of gridded hydraulic head}
+#'   \item{\code{flow}: }{A data.frame of flow with gridded \code{x} and \code{y}, along with \code{dx} and
+#'   \code{dy} from \code{get_flow_dir()}, and \code{x2} and \code{y2} which are specifically
+#'   intended for plotting using \code{geom_segment()}-- they are normalized so that
+#'   the maximum value is equal to the grid spacing.}
+#' }
+#' @export
+#' @examples
+#' # define aquifer
+#' bounds_df <- data.frame(bound_type=c("CH","NF","NF","NF"),m=c(Inf,0,Inf,0),b=c(0,1000,1000,0))
+#' aquifer_unconfined <- define_aquifer("unconfined",1e-3,bounds=bounds_df,h0=100)
+#'
+#' # define wells and well images
+#' set.seed(30)
+#' wells_df <- data.frame(x=runif(8,0,1000),y=runif(8,0,1000),diam=1) %>%
+#'   mutate(R=1000,Q=-1/n())
+#' wells <- define_wells(wells_df) %>% generate_image_wells(aquifer_unconfined)
+#'
+#' gridded <- get_gridded_hydrodynamics(wells,aquifer_unconfined,c(20,20),c(8,8))
+#'
+#' ggplot() +
+#'   geom_raster(data=gridded$head,aes(x,y,fill=head_m)) +
+#'   geom_segment(data=gridded$flow,aes(x,y,xend=x2,yend=y2),
+#'                arrow = arrow(ends="last",type="closed",length=unit(1,"mm")),color="black") +
+#'   geom_segment(data=aquifer_unconfined$bounds,aes(x1,y1,xend=x2,yend=y2,color=bound_type)) +
+#'   geom_point(data=wells %>% filter(wID==orig_wID),aes(x,y),shape=21) +
+#'   coord_equal()
+get_gridded_hydrodynamics <- function(wells,aquifer,head_dim=c(20,20),flow_dim=c(10,10)) {
+
+  grid_bounds <- get_quad_vertices(aquifer$bounds) %>% dplyr::filter(!is.na(x)) %>%
+    dplyr::summarize(xmin=min(x),xmax=max(x),ymin=min(y),ymax=max(y))
+  grid_pts_head <- with(grid_bounds,tidyr::crossing(x=seq(xmin,xmax,length.out=head_dim[1]),
+                                             y=seq(ymin,ymax,length.out=head_dim[2])))
+  heads <- get_hydraulic_head(grid_pts_head,wells,aquifer)
+  head_df <- grid_pts_head %>% dplyr::bind_cols(head_m=heads)
+
+  flow_x_seq <- with(grid_bounds,seq(xmin,xmax,length.out=flow_dim[1]))
+  flow_y_seq <- with(grid_bounds,seq(ymin,ymax,length.out=flow_dim[2]))
+  flow_grid_dx <- flow_x_seq[2] - flow_x_seq[1]
+  flow_grid_dy <- flow_y_seq[2] - flow_y_seq[1]
+  grid_spacing <- min(flow_grid_dx,flow_grid_dy)
+  grid_pts_flowdir <- tidyr::crossing(x=flow_x_seq,y=flow_y_seq)
+  flow_dir <- get_flowdir(grid_pts_flowdir,wells,aquifer)
+  # the dx_norm and dy_norm are used to make the arrows visible in the plot that follows.
+  flow_dir_df <- grid_pts_flowdir %>% dplyr::bind_cols(flow_dir) %>%
+    dplyr::mutate(angle=atan(dy/dx),
+           mag=sqrt(dx^2+dy^2),
+           mag_max=max(mag),
+           flow_scaling=grid_spacing/mag_max,
+           mag_norm=mag*flow_scaling, # this scales the flow for the arrow plot
+           dx_norm=cos(angle)*sign(dx)*mag_norm,
+           dy_norm=sin(angle)*sign(dx)*mag_norm,
+           x2=x+dx_norm,
+           y2=y+dy_norm) %>%
+    dplyr::select(x,y,dx,dy,x2,y2)
+  return(list(head=head_df,flow=flow_dir_df))
+}
+
+
--- tests/testthat/test_wrappers.R
+++ tests/testthat/test_wrappers.R
@@ -16,3 +16,23 @@ df <- tibble::tibble(var=c("PHI_A_A", "PHI_A_B", "PHI_B_A", "PHI_B_B"),
 test_that("get_pumping_relationships returns correct result for simple aquifer and two countries",{
   expect_equal(get_drawdown_relationships(wells,aquifer_unconfined,country,weights) %>% dplyr::mutate(pot=round(pot,4)) %>% dplyr::select(var,pot),df)
 })
+
+
+# define aquifer
+bounds_df <- data.frame(bound_type=c("CH","NF","NF","NF"),m=c(Inf,0,Inf,0),b=c(0,1000,1000,0))
+aquifer_unconfined <- define_aquifer("unconfined",1e-3,bounds=bounds_df,h0=100)
+
+# define wells and well images
+set.seed(30)
+wells_df <- data.frame(x=runif(8,0,1000),y=runif(8,0,1000),diam=1) %>% dplyr::mutate(R=1000,Q=-1/dplyr::n())
+wells <- define_wells(wells_df) %>% generate_image_wells(aquifer_unconfined)
+gridded <- get_gridded_hydrodynamics(wells,aquifer_unconfined,c(15,15),c(8,8))
+test_that("get_gridded_hydrodynamics returns accurate dimensions for head",{
+  expect_equal(dim(gridded$head),c(225,3))
+})
+test_that("get_gridded_hydrodynamics returns accurate dimensions for flow",{
+  expect_equal(dim(gridded$flow),c(64,6))
+})
+
+
+
