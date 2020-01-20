530b6f38e6de3e42e0be9d182f78006fd478bb4d
Gopal <gopalpenny@gmail.com>
2020-01-20 13:53:28 -0500

    updated genevese_wells.rda

--- data-raw/genevese_wells.R
+++ data-raw/genevese_wells.R
@@ -1,7 +1,13 @@
 ## code to prepare `genevese_wells` dataset goes here
 
 
+set.seed(100)
 genevese_wells <- sf::read_sf("/Users/gopal/Projects/CNH/genevois/spatial/shp/stylized_aquifer/genevois_wells_stylized.shp") %>%
-  mutate(diam=RADUIS*2) %>% select(diam,well_group=CONCESSION)
+  dplyr::mutate(diam=round(runif(dplyr::n(),0.5,2),1)) %>%
+  dplyr::select(diam,well_group=CONCESSION) %>%
+  dplyr::group_by(well_group) %>%
+  dplyr::sample_frac(0.55) %>%
+  dplyr::group_by()
+# ggplot() + geom_sf(data=genevese_wells,aes(color=well_group))
 
 usethis::use_data(genevese_wells)
