998d274b5423296a11d8edcd091f7427287640de
Gopal <gopalpenny@gmail.com>
2020-01-29 22:34:21 -0500

    minor fixes when checking class(loc)

--- R/anem_recharge.R
+++ R/anem_recharge.R
@@ -1,15 +1,22 @@
 # anem_recharge.R
 
-#' Get hydraulic potential due to recharge
+#' Get undisturbed hydraulic potential due to recharge
 #'
-#' Get hydraulic potential due to recharge
+#' Get undisturbed hydraulic potential due to recharge. This is the water table potential
+#' (head or discharge potential) without any well pumping.
+#'
+#' Note that this function ignores any boundaries, meaning
+#' the user must ensure that the recharge does not conflict with boundaries. For example,
+#' if uniform flow is specified, it will pass through any boundary, including no-flow boundaries.
 #' @param loc Location for evaluating Vector location as c(x,y) or data.frame containing x, y columns
 #' @param Aquifer Aquifer containing containing \code{h0} and \code{recharge$params} (see Details)
 #' @details
 #' The aquifer and aquifer$recharge must be correctly specified using \code{define_aquifer} and/or \code{define_recharge}.
 #' @export
 #' @return
-#' Returns hydraulic potential at the location(s) using helper functions for each recharge_type / aquifer_type:
+#' Returns hydraulic potential at the location(s) using helper functions for each recharge_type / aquifer_type.
+#' If aquifer_type=="confined", hydraulic head is returned. If it is "unconfined", discharge potential is returned.
+#' Note that these functions are not exported to the package namespace:
 #' \itemize{
 #' \item 1. Constant flow, "F": \code{get_recharge_flow_potential()}
 #' \item 2. Head boundaries, "H": not yet implemented
@@ -19,19 +26,19 @@
 #' ## Flow - confined aquifer
 #' recharge_params <- list(recharge_type="F",recharge_vector=c(0,0,1,1),flow=1,x0=0,y0=0)
 #' aquifer <- define_aquifer("confined",1,h0=0,z0=1,recharge=recharge_params)
-#' get_recharge_hydraulic_potential(c(1/sqrt(2),1/sqrt(2)), aquifer)
+#' get_recharge_undisturbed_potential(c(1/sqrt(2),1/sqrt(2)), aquifer)
 #'
 #' loc <- expand.grid(x=-2:2,y=-2:2)
-#' loc$h <- get_recharge_hydraulic_potential(loc, aquifer)
+#' loc$h <- get_recharge_undisturbed_potential(loc, aquifer)
 #' library(ggplot2)
 #' ggplot(loc) + geom_raster(aes(x,y,fill=h)) + scale_fill_gradient2()
 #'
 #' recharge_params <- list(recharge_type="F",recharge_vector=c(-1,-5,0,0),flow=1,x0=0,y0=0)
 #' aquifer <- define_aquifer("confined",1,h0=10,z0=1,recharge=recharge_params)
 #' loc <- expand.grid(x=-100:100,y=-100:100)
-#' loc$h <- get_recharge_hydraulic_potential(loc, aquifer)
+#' loc$h <- get_recharge_undisturbed_potential(loc, aquifer)
 #' ggplot(loc) + geom_raster(aes(x,y,fill=h)) + scale_fill_gradient2()
-get_recharge_hydraulic_potential <- function(loc, aquifer) {
+get_recharge_undisturbed_potential <- function(loc, aquifer) {
 
   # 1. Recharge type: Flow
   if (aquifer$recharge$recharge_type == "F") {
@@ -48,6 +55,8 @@ get_recharge_hydraulic_potential <- function(loc, aquifer) {
     # 3a. Confined, 3b. Unconfined aquifers
     stop("Head boundaries not yet implemented.")
     pot <- get_recharge_divide_potential(loc, aquifer)
+  } else {
+    stop("aquifer$recharge exists but not specified as F, D, or H")
   }
 
   if (is.null(pot)) {
@@ -105,7 +114,7 @@ get_recharge_flow_potential <- function(loc, aquifer) {
 
   # 1b Confined flow
   if (aquifer$aquifer_type=="confined") {
-    if (class(loc) == "numeric") {
+    if (identical(class(loc),"numeric") | identical(class(loc),"integer")) {
       pot <- aquifer$h0 + params$x_term * (loc[1] - params$x0) + params$y_term * (loc[2] - params$y0)
     } else if (any(grepl("data.frame",class(loc)))) {
       pot <- aquifer$h0 + params$x_term * (loc$x - params$x0) + params$y_term * (loc$y - params$y0)
@@ -150,9 +159,10 @@ get_recharge_flow_potential <- function(loc, aquifer) {
 #' Returns hydraulic head (confined aquifers) or discharge potential (unconfined aquifers) at the location(s).
 #' @examples
 #' ## Flow - confined aquifer
-#' recharge_params <- list(recharge_type="D",recharge_vector=c(0,0,1,2),flow=1,x0=0,y0=0)
+#' recharge_params <- list(recharge_type="D",recharge_vector=c(0,0,1,sqrt(3)),flow_main=1,flow_opp=2,x0=0,y0=0)
 #' aquifer <- define_aquifer("confined",1,h0=0,z0=1,recharge=recharge_params)
-#' get_recharge_divide_potential(c(1/sqrt(2),1/sqrt(2)), aquifer)
+#' get_recharge_divide_potential(c(1/2,sqrt(3)/2), aquifer)
+#' get_recharge_divide_potential(c(-1/2,-sqrt(3)/2), aquifer)
 #'
 #' loc <- expand.grid(x=-2:2,y=-2:2)
 #' loc$h <- get_recharge_divide_potential(loc, aquifer)
@@ -188,12 +198,12 @@ get_recharge_divide_potential <- function(loc, aquifer) {
   params <- aquifer$recharge
 
   # 1b Confined flow
-    if (class(loc) == "numeric") {
+    if (identical(class(loc),"numeric") | identical(class(loc),"integer")) {
       # Check which side of the divide the point is on -- main or opposite
       # Point is on main side (direction of recharge_vector) IF:
       # (a) y_side == main_side_y, OR (b) abs(m) == Inf & sign(x - b) ==
       # Otherwise, the point is on the divide or on the opposite side.
-      y_side <- sign(loc[2] - params$divide_m * loc[1] + params$divide_b)
+      y_side <- sign(loc[2] - (params$divide_m * loc[1] + params$divide_b))
       y_side <- ifelse(is.nan(y_side),Inf,y_side)
       x_side_Inf <- sign(loc[1] - params$divide_b)
       main_side <- y_side == params$main_side_y | (x_side_Inf == params$main_side_x & abs(params$divide_m) == Inf)
