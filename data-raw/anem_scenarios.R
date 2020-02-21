a73f35624083fe05bcca8f9a19e56f331f5bb9bf
Gopal <gopalpenny@gmail.com>
2020-02-21 15:53:58 -0500

    updated aquifer_example

--- data-raw/anem_scenarios.R
+++ data-raw/anem_scenarios.R
@@ -11,6 +11,6 @@ wells_example <- data.frame(x=runif(8,0,1000),y=runif(8,0,1000),diam=1) %>%
   mutate(weights=1,Q=-1/n()) %>% group_by()
 
 bounds_example <- data.frame(bound_type=c("CH","NF","NF","NF"),m=c(Inf,0,Inf,0),b=c(0,1000,1000,0))
-aquifer_example <- define_aquifer("unconfined",1e-3,bounds=bounds_example,h0=100,z0=10)
+aquifer_example <- define_aquifer("unconfined",1e-3,bounds=bounds_example,h0=100,z0=10,n=0.35)
 
 usethis::use_data(wells_example,bounds_example,aquifer_example,overwrite = TRUE)
