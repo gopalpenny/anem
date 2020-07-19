cd0d1fd455b9e007af2738062c8b6ce45e122a8a
Gopal Penny <gopalpenny@gmail.com>
2020-07-18 21:11:31 -0400

    updated app directory for shiny tests

--- inst/anem-app/tests/run_tests_script.R
+++ inst/anem-app/tests/run_tests_script.R
@@ -1,12 +1,17 @@
+app_dir <- "."
+# app_dir <- "inst/anem-app/tests/"
+
 # test_gw_district -- note: PNG files may not match exactly
-testApp("inst/anem-app/tests/", "test_gw_district")
+testApp(app_dir, "test_gw_district")
 
 # test_anem_app_basics -- note: PNG files may not match exactly
-testApp("inst/anem-app/tests/", "test_anem_app_prep")
+testApp(app_dir, "test_anem_app_prep")
 
 # test_anem_app_well_capture -- note: PNG files may not match exactly
-testApp("inst/anem-app/tests/", "test_anem_app_well_capture")
+testApp(app_dir, "test_anem_app_well_capture")
 
 # test_anem_app_well_capture -- note: PNG files may not match exactly
-testApp("inst/anem-app/tests/", "test_particles")
+testApp(app_dir, "test_particles")
 
+# test_gwdistricts_well_capture -- note: PNG files may not match exactly
+testApp(app_dir, "test_gwdistricts_well_capture")
